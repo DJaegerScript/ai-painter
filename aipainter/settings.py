2ec749065d20481e496b6036018d4630525a768d
Adjie DJaka Permana <adjie.djaka@ui.ac.id>
2021-12-15 13:46:35 +0700

    removed heroku configuration

--- aipainter/settings.py
+++ aipainter/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,5 +125,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, "")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
