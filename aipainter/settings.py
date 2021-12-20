509253293e07b51402c47d97d4838775f9dbfb33
Adjie DJaka Permana <adjie.djaka@ui.ac.id>
2021-12-20 20:35:22 +0700

    allowed frame access

--- aipainter/settings.py
+++ aipainter/settings.py
@@ -124,3 +124,7 @@ django_heroku.settings(locals())
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+X_FRAME_OPTIONS = 'ALLOWALL'
+
+XS_SHARING_ALLOWED_METHODS = ['POST','GET','OPTIONS', 'PUT', 'DELETE']
